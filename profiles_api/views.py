eeae33867a20b0d112ac10a01828e58b239fdbdd
Pankaj Kumar Ray <800pankaj@gmail.com>
2021-06-19 21:48:34 +0530

    Added HelloApiView to handle the endpoint

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+    
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'User HTTP methods as function (get, post, put, delete)',
+            'Hello world!',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
